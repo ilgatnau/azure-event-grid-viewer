ac38f9ce6b7b61fe74ffc4b8fd033bd658a02b6a
Ivan Gatnau Lopez <ilgatnau@gmail.com>
2020-04-07 12:47:45 +0100

    new listener class

---
+++ viewer/Controllers/ListenerController.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.IO;
+using System.Net;
+using System.Text;
+using System.Net.Http;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Microsoft.AspNetCore.SignalR;
+using viewer.Hubs;
+using viewer.Models;
+
+using System.Text;
+using System.Diagnostics;
+using System.Threading;
+
+using Microsoft.Azure; // Namespace for CloudConfigurationManager
+using Microsoft.Azure.Storage; // Namespace for CloudStorageAccount
+using Microsoft.Azure.Storage.Queue; // Namespace for Queue storage types
+
+
+
+namespace viewer.Controllers
+{
+    public class ListenerController : Controller
+    {
+        private readonly IHubContext<GridEventsHub> _hubContext;
+
+        public ListenerController(IHubContext<GridEventsHub> gridEventsHubContext)
+        {
+            this._hubContext = gridEventsHubContext;
+            
+            traceMessage("Before configuring queue");
+            ConfigureQueue();
+        }
+
+        private static void ConfigureQueue()
+        {
+            traceMessage("Connecting to queue");
+
+            try
+            {
+                // Retrieve storage account from connection string.
+                CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=stgaccprivateendpoint;AccountKey=YGQ7LP81FSOZnQm3RNxv6UeAI+O+EEHyNQpTem0Rh/83N/q6T4ZSki4RQbaS8Mv+judJiUs9Z69pIls0BnCZeg==;EndpointSuffix=core.windows.net");
+
+                // Create the queue client.
+                CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
+
+                // Retrieve a reference to a container.
+                CloudQueue queue = queueClient.GetQueueReference("blobupload");
+
+                traceMessage("Adquired queue");
+
+                // Create the queue if it doesn't already exist
+                queue.CreateIfNotExists();
+
+                int currentBackoff = 0;
+                int maximumBackoff = 10;
+
+                while (true)
+                {
+                    var message = queue.GetMessage();
+                    if (message != null)
+                    {
+                        // Reset backoff
+                        currentBackoff = 0;
+
+                        // Process the message
+                        Console.ForegroundColor = ConsoleColor.Green;
+                        Console.WriteLine(
+                        "Processed message with contents: {0}", message.AsString);
+                        // Mark completed
+                        queue.DeleteMessage(message);
+                    }
+                    else
+                    {
+                        if (currentBackoff < maximumBackoff)
+                        {
+                            currentBackoff++;
+                        }
+                        Console.ForegroundColor = ConsoleColor.Blue;
+                        Console.WriteLine("Backing off for {0} seconds...", currentBackoff);
+                        Thread.Sleep(TimeSpan.FromSeconds(currentBackoff));
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Trace.WriteLine(e);
+                Console.WriteLine(e);
+            }
+
+            
+        }
+
+        private static void traceMessage(string message) {
+            Console.WriteLine(message);
+            Trace.WriteLine(message);
+        }
+    }
+}
--- viewer/Program.cs
+++ viewer/Program.cs
@@ -23,62 +23,12 @@ namespace viewer
         public static void Main(string[] args)
         {
             CreateWebHostBuilder(args).Build().Run();
-
-            traceMessage("Before configuring queue");
-            ConfigureQueue();
         }
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
                 .UseStartup<Startup>();
-                
-        private static void ConfigureQueue()
-        {
-            traceMessage("Connecting to queue");
-
-            try
-            {
-                // Retrieve storage account from connection string.
-                CloudStorageAccount storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=stgaccprivateendpoint;AccountKey=YGQ7LP81FSOZnQm3RNxv6UeAI+O+EEHyNQpTem0Rh/83N/q6T4ZSki4RQbaS8Mv+judJiUs9Z69pIls0BnCZeg==;EndpointSuffix=core.windows.net");
-
-                // Create the queue client.
-                CloudQueueClient queueClient = storageAccount.CreateCloudQueueClient();
-
-                // Retrieve a reference to a container.
-                CloudQueue queue = queueClient.GetQueueReference("blobupload");
-
-                traceMessage("Adquired queue");
-
-                // Create the queue if it doesn't already exist
-                queue.CreateIfNotExists();
-
-                // Peek at the next message
-                CloudQueueMessage peekedMessage = queue.PeekMessage();
-
-                // Display message.
-                traceMessage(peekedMessage.AsString);
-
-                CloudQueueMessage message = queue.GetMessage();
-
-                message.SetMessageContent2("Updated contents.", false);
-                queue.UpdateMessage(message,
-                        TimeSpan.FromSeconds(60.0),  // Make it invisible for another 60 seconds.
-                        MessageUpdateFields.Content | MessageUpdateFields.Visibility);
-            }
-            catch (Exception e)
-            {
-                Trace.WriteLine(e);
-                Console.WriteLine(e);
-            }
-
-            
-        }
-
-        private static void traceMessage(string message) {
-            Console.WriteLine(message);
-            Trace.WriteLine(message);
-        }
     }
-
+                
 
 }
